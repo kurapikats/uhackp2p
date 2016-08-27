e651e6261bfbc4d7dd19e0b3a84874c68ea25d50
Jr Reyes <jr.evilboss@gmail.com>
2016-08-27 13:53:15 +0800

    added new method for api

--- u-tang/client/modules/api/routes.jsx
+++ u-tang/client/modules/api/routes.jsx
@@ -4,14 +4,14 @@ import {mount} from 'react-mounter';
 import MainLayout from '/client/modules/core/components/main_layout.jsx';
 
 export default function (injectDeps, {FlowRouter}) {
-  const MainLayoutCtx = injectDeps(MainLayout);
-
-  // FlowRouter.route('', {
-  //   name: '',
-  //   action() {
-  //     mount(MainLayoutCtx, {
-  //       content: () => (< />)
-  //     });
-  //   }
-  // });
+	const MainLayoutCtx = injectDeps(MainLayout);
+	
+	FlowRouter.route('/api', {
+		name: '',
+		action() {
+			mount(MainLayoutCtx, {
+				content: () => (< />)
+			});
+		}
+	});
 }
---
+++ u-tang/server/methods/api.js
@@ -0,0 +1,11 @@
+import {Api} from '/lib/collections';
+import {Meteor} from 'meteor/meteor';
+import {check} from 'meteor/check';
+
+export default function () {
+  Meteor.methods({
+    'api.sample'() {
+
+    }
+  });
+}
