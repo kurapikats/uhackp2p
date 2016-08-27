d68f068bff4a130080a49f16f35a2029a9da297c
kurapikats <kurapikats@yahoo.com>
2016-08-28 05:56:53 +0800

    update dashboard and profile ui

--- u-tang/client/modules/users/components/dashboard.jsx
+++ u-tang/client/modules/users/components/dashboard.jsx
@@ -1,14 +1,50 @@
 import React from 'react';
+import {MobileTearSheet} from 'material-ui';
+import {List, ListItem} from 'material-ui/List';
+import ContentInbox from 'material-ui/svg-icons/content/inbox';
+import ActionGrade from 'material-ui/svg-icons/action/grade';
+import ContentSend from 'material-ui/svg-icons/content/send';
+import ContentDrafts from 'material-ui/svg-icons/content/drafts';
+import Divider from 'material-ui/Divider';
+import ActionInfo from 'material-ui/svg-icons/action/info';
 
 class Dashboard extends React.Component {
   constructor(props) {
     super(props);
   }
 
+  goToProfile() {
+    FlowRouter.go('/profile');
+  }
+
+  goToThirdParty() {
+    FlowRouter.go('/profile');
+  }
+
+  goToBorrowers() {
+    FlowRouter.go('/borrowers');
+  }
+
+  goToPending() {
+    FlowRouter.go('/pendings');
+  }
+
+  goToTransactions() {
+    FlowRouter.go('/transactions');
+  }
+
   render() {
     return (
       <section className="dashboard">
-        <div>Dashboard Page</div>
+        <h4>Dashboard</h4>
+
+        <List>
+          <ListItem onTouchTap={this.goToProfile} primaryText="My Accounts" leftIcon={<ContentInbox />} />
+          <ListItem onTouchTap={this.goToThirdParty} primaryText="Enroll Third Party Accounts" leftIcon={<ActionGrade />} />
+          <ListItem onTouchTap={this.goToBorrowers} primaryText="List of Borrowers" leftIcon={<ContentSend />} />
+          <ListItem onTouchTap={this.goToPending} primaryText="Pending Requests" leftIcon={<ContentDrafts />} />
+          <ListItem onTouchTap={this.goToTransactions} primaryText="Transaction History" leftIcon={<ContentInbox />} />
+        </List>
       </section>
     );
   }
--- u-tang/client/modules/users/components/profile.jsx
+++ u-tang/client/modules/users/components/profile.jsx
@@ -10,11 +10,11 @@ class Profile extends React.Component {
     console.log(data);
     return (
       <section className="profile">
-        <div> Profile Page </div>
+        <h4> Profile Page </h4>
 
         <div> <labe> Name :</labe> Sample Name </div>
         <div> <labe> Email :</labe> {data? data.emails[0].address : "Sample Email"} </div>
-        <div> <labe> Card ID :</labe> {data ? data.profile.cardId : "Sample card Id"} </div>
+        <div> <labe> Account No. :</labe> {data ? data.profile.cardId : "Sample card Id"} </div>
         <div> <labe> Balance :</labe> Sample Balance </div>
       </section>
     );
