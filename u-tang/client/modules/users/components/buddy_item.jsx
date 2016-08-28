0f0cc8e0c226578d52c837a62ad37a39043bac11
Jr Reyes <jr.evilboss@gmail.com>
2016-08-28 10:38:56 +0800

    Added details to buddy list

--- u-tang/client/modules/users/components/buddy_item.jsx
+++ u-tang/client/modules/users/components/buddy_item.jsx
@@ -34,7 +34,7 @@ class BuddyItem extends React.Component {
 		return (
 			<Card expanded={this.state.expanded} onExpandChange={this.handleExpandChange}>
 				<CardHeader
-					title={buddy.firstName+' '+buddy.lastName}
+					title={buddy.firstName + ' ' + buddy.lastName}
 					avatar={buddy.displayPhoto}
 					actAsExpander={true}
 				/>
@@ -43,20 +43,21 @@ class BuddyItem extends React.Component {
 				</CardText>
 				<CardMedia
 					expandable={true}
-					overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle"/>}
+					overlay={<CardTitle title="User Info"/>}
 				>
 					<img src="images/nature-600-337.jpg"/>
 				</CardMedia>
-				<CardTitle title="Card title" subtitle="Card subtitle" expandable={true}/>
+				<CardTitle title={buddy.firstName + ' ' + buddy.lastName} subtitle="Stats" expandable={true}/>
 				<CardText expandable={true}>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-					Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-					Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-					Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+					<h5>Loans requested</h5>
+					<h5>Loans approved</h5>
+					<h5>Pending Transactions</h5>
+					<h5>Transactions Done</h5>
+					<h5>Payback Rating</h5>
 				</CardText>
 				<CardActions>
-					{(!this.state.expanded)?
-						<FlatButton label="More info" onTouchTap={this.handleExpand}/>:
+					{(!this.state.expanded) ?
+						<FlatButton label="More info" onTouchTap={this.handleExpand}/> :
 						<FlatButton label="close" onTouchTap={this.handleReduce}/>
 					}
 				</CardActions>
