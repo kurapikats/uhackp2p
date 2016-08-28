3640da320ca9a7f8bbcaa3343082f561abe50c9b
roland <roland.cabalhin@gmail.com>
2016-08-28 11:34:30 +0800

    added modal

--- u-tang/client/modules/users/components/buddy_item.jsx
+++ u-tang/client/modules/users/components/buddy_item.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
-import FlatButton from 'material-ui/FlatButton';
+import {TextField, Dialog, FlatButton} from 'material-ui';
 import Toggle from 'material-ui/Toggle';
 
 class BuddyItem extends React.Component {
@@ -9,12 +9,43 @@ class BuddyItem extends React.Component {
 		super(props);
 		this.state = {
 			expanded: false,
+			requestLoan: false,
+			message: null,
+			amount: null
 		};
 	}
+
+	_update(e){
+		let val = e.currentTarget.value;
+		let name = e.currentTarget.name;
+
+		switch(name){
+			case 'amount':
+				this.setState({amount: val});
+				break;
+			case 'message':
+				this.setState({message: val});
+				break;
+		}
+	}
+
+	send(){
+		const buddy = this.props.buddy.profile;
+		console.log(buddy, this.state);
+	};
 	
 	handleExpandChange = (expanded) => {
 		this.setState({expanded: expanded});
 	};
+
+	openField(){
+		this.setState({requestLoan: true});
+	}
+
+	handleLoanReduce() {
+		this.setState({requestLoan: false
+		})
+	}
 	
 	handleToggle = (event, toggle) => {
 		this.setState({expanded: toggle});
@@ -30,7 +61,7 @@ class BuddyItem extends React.Component {
 	
 	render() {
 		const buddy = this.props.buddy.profile;
-		console.log(buddy);
+		//console.log(buddy);
 		return (
 			<Card expanded={this.state.expanded} onExpandChange={this.handleExpandChange}>
 				<CardHeader
@@ -53,6 +84,8 @@ class BuddyItem extends React.Component {
 					<h5>Pending Transactions</h5>
 					<h5>Transactions Done</h5>
 					<h5>Payback Rating</h5>
+
+					<FlatButton label="Request Loan" onTouchTap={this.openField.bind(this)}/>
 				</CardText>
 				<CardActions>
 					{(!this.state.expanded) ?
@@ -60,6 +93,45 @@ class BuddyItem extends React.Component {
 						<FlatButton label="close" onTouchTap={this.handleReduce}/>
 					}
 				</CardActions>
+
+				<Dialog
+					title="Borrow from XYZ UserName"
+					modal={true}
+					open={this.state.requestLoan}
+				>
+					<p>Borrow from Xyz User</p>
+					<TextField
+						ref="amount"
+						name="amount"
+						hintText="Amount"
+						floatingLabelText="Amount"
+						className="inputLeftMargin"
+						fullWidth={true}
+						underlineShow={true}
+						onChange={this._update.bind(this)}
+						required
+					/><br/>
+					<TextField
+						hintText="Message"
+						name="message"
+						multiLine={true}
+						rows={1}
+						rowsMax={5}
+						fullWidth={true}
+						onChange={this._update.bind(this)}
+					/><br />
+
+					<FlatButton
+						label="Cancel"
+						primary={false}
+						onTouchTap={this.handleLoanReduce.bind(this)}
+					/>
+					<FlatButton
+						label="Send Request"
+						primary={true}
+						onTouchTap={this.send.bind(this)}
+					/>
+				</Dialog>
 			</Card>
 		);
 	}
